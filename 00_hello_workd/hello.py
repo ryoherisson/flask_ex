b42ca2bb422a345631bdc56b78f330b889b2d73d
ryoherisson <ryhizw@gmail.com>
2020-04-05 22:16:10 +0900

    first commit

--- 00_hello_workd/hello.py
+++ 00_hello_workd/hello.py
@@ -1,13 +1,13 @@
-from flask import Flask
+# from flask import Flask
 
-app = Flask(__name__)
+# app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hellow World!"
+# @app.route('/')
+# def hello_world():
+#     return "Hellow World!"
 
-if __name__ == "__main__":
-    # docker run時に、-p 外部からアクセスされるポート番号:コンテナ側のポート番号を指定
-    # 今回の場合は、8008:5000
-    # localからlocalhost:8008でアクセス
-    app.run(host='0.0.0.0', port=5000, debug=True)
+# if __name__ == "__main__":
+#     # docker run時に、-p 外部からアクセスされるポート番号:コンテナ側のポート番号を指定
+#     # 今回の場合は、8008:5000
+#     # localからlocalhost:8008でアクセス
+#     app.run(host='0.0.0.0', port=5000, debug=True)
