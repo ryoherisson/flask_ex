0d82c2ebdd67dd69bcb461e9c21b00334dba2f1f
ryoherisson <ryhizw@gmail.com>
2020-04-05 22:49:12 +0900

    add index.html

--- 01_flask_blog/flask_blog/views.py
+++ 01_flask_blog/flask_blog/views.py
@@ -1,8 +1,15 @@
 # flask_blog/views.py
+from flask import flash
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import session
+from flask import url_for
 
 from flask_blog import app
 
 @app.route('/')
 def show_entries():
-    return "Hello World!"
+    # index.htmlでrendering
+    return render_template('entries/index.html')
 
