c32a0d7fc0932a7222d4add83b5850898afe2e63
ryoherisson <ryhizw@gmail.com>
2020-04-06 00:00:26 +0900

    add sqlalchemy

--- 01_flask_blog/flask_blog/config.py
+++ 01_flask_blog/flask_blog/config.py
@@ -1,5 +1,8 @@
 # flask/config.py
 
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:///flask_blog.db'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
 DEBUG = True
 # SECRET_KEY should be longer and randomly set.
 SECRET_KEY = 'secret key'
